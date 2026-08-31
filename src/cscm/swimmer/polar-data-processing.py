ee3ffc33efb639c0e0627af4ba9869a4054e3d68
Marc Portier <marc.portier@gmail.com>
2026-08-31 16:24:31 +0200

    linting

--- src/cscm/swimmer/polar-data-processing.py
+++ src/cscm/swimmer/polar-data-processing.py
@@ -1,4 +1,4 @@
 # src/cscm/swimmer/polar-data-processing.py
 
-## scripts to extract and proces polar smartwatch export data
+# scripts to extract and proces polar smartwatch export data
 # go to https://account.polar.com/#export to get your own data

db9c4fa05bdefdf12c2b4ff608ef1bbc5be19ab2
Marc Portier <marc.portier@gmail.com>
2026-09-01 23:30:38 +0200

    new list of coordinates -- introduce "proxy" points to avoid 'boundary cells'

--- src/cscm/wellknown.py
+++ src/cscm/wellknown.py
@@ -6,33 +6,142 @@ import random
 
 # Positions
 POSITIONS = dict(
-    NIEUWPOORT=LabeledPosition(
-        label="Nieuwpoort",
-        lat=51.15667, lon=2.72333),
+    # WKT,name,description
+    # "POINT (2.9704106714146716 51.25743146659003)",Bredene Post 4,
     BREDENE_POST4=LabeledPosition(
         label="Bredene Reddingspost 4",
-        lat=51.25743146659003, lon=2.9704106714146716),
+        lat=51.25743146659003, lon=2.9704106714146716
+    ),
+    # "POINT (2.9560906 51.2729652)",Bredene Post 4 Proxy,
+    BREDENE_POST4_PROXY=LabeledPosition(
+        label="Bredene Reddingspost 4 Proxy",
+        lat=51.2729652, lon=2.9560906
+    ),
+    # "POINT (2.62575 51.1194)",Koksijde,
     KOKSIJDE=LabeledPosition(
         label="Koksijde",
-        lat=51.11940, lon=2.62575),
-    KOKSIJDE_END_316_HT=LabeledPosition(
-        label="From Koksijde (projected end of swim 316° at HT)",
-        lat=51.264652, lon=2.402563),
-    KOKSIJDE_END_333_LT=LabeledPosition(
-        label="From Koksijde (projected end of swim 333° at LT)",
-        lat=51.298858, lon=2.479862),
-    ORFORD=LabeledPosition(
+        lat=51.11940, lon=2.62575
+    ),
+    # "POINT (2.6101006 51.1345606)",Koksijde Proxy,
+    KOKSIJDE_PROXY=LabeledPosition(
+        label="Koksijde Proxy",
+        lat=51.1345606, lon=2.6101006
+    ),
+    # "POINT (2.72333 51.15667)",Nieuwpoort,
+    NIEUWPOORT=LabeledPosition(
+        label="Nieuwpoort",
+        lat=51.15667, lon=2.72333
+    ),
+    # "POINT (2.707728 51.1711833)",Nieuwpoort Proxy,
+    NIEUWPOORT_PROXY=LabeledPosition(
+        label="Nieuwpoort Proxy",
+        lat=51.1711833, lon=2.707728
+    ),
+    # "POINT (1.58333 52.08333)",Orford Ness,
+    ORFORD_NESS=LabeledPosition(
         label="Orford Ness",
-        lat=52.08333, lon=1.58333),
+        lat=52.08333, lon=1.58333
+    ),
+    # "POINT (1.6081401 52.0736728)",Orford Ness Proxy,
+    ORFORD_NESS_PROXY=LabeledPosition(
+        label="Orford Ness Proxy",
+        lat=52.0736728, lon=1.6081401
+    ),
+    # "POINT (1.4451754 51.3836514)",Kingsgate,
     KINGSGATE=LabeledPosition(
-        label="Kingsgate Bay",
-        lat=51.38333, lon=1.44333),
+        label="Kingsgate",
+        lat=51.3836514, lon=1.4451754
+    ),
+    # "POINT (1.4707437 51.3914205)",Kingsgate Proxy,
+    KINGSGATE_PROXY=LabeledPosition(
+        label="Kingsgate Proxy",
+        lat=51.3914205, lon=1.4707437
+    ),
+    # "POINT (1.3060091 51.1132473)",Dover,Shakespeare Beach
     DOVER=LabeledPosition(
-        label="Shakespeare Beach, Dover",
-        lat=51.1130669, lon=1.2998859),
-    CAPGRISNEZ=LabeledPosition(
-        label="Cap Gris-Nez, Frankrijk",
-        lat=50.925668, lon=1.706695),
+        label="Dover, Shakespeare Beach",
+        lat=51.1132473, lon=1.3060091
+    ),
+    # "POINT (1.3204159 51.0976191)",Dover Proxy,
+    DOVER_PROXY=LabeledPosition(
+        label="Dover Proxy",
+        lat=51.0976191, lon=1.3204159
+    ),
+    # "POINT (1.6962061 50.9202391)",Cap Blanc Nez,
+    CAP_BLANC_NEZ=LabeledPosition(
+        label="Cap Blanc Nez",
+        lat=50.9202391, lon=1.6962061
+    ),
+    # "POINT (1.6716473 50.9298626)",Cap Blanc Nez Proxy,
+    CAP_BLANC_NEZ_PROXY=LabeledPosition(
+        label="Cap Blanc Nez Proxy",
+        lat=50.9298626, lon=1.6716473
+    ),
+    # "POINT (1.5585258 50.9823472)",W 1/3,
+    W_1_3=LabeledPosition(
+        label="W 1/3",
+        lat=50.9823472, lon=1.5585258
+    ),
+    # "POINT (1.4459964 51.0374139)",W 2/3,
+    W_2_3=LabeledPosition(
+        label="W 2/3",
+        lat=51.0374139, lon=1.4459964
+    ),
+    # "POINT (1.5028793 51.0096289)",W 1/2,
+    W_1_2=LabeledPosition(
+        label="W 1/2",
+        lat=51.0096289, lon=1.5028793
+    ),
+    # "POINT (1.857809 51.3099446)",M 2/3,
+    M_2_3=LabeledPosition(
+        label="M 2/3",
+        lat=51.3099446, lon=1.857809
+    ),
+    # "POINT (2.2672317 51.2410691)",M 1/3,
+    M_1_3=LabeledPosition(
+        label="M 1/3",
+        lat=51.2410691, lon=2.2672317
+    ),
+    # "POINT (2.0602783 51.2749006)",M 1/2,
+    M_1_2=LabeledPosition(
+        label="M 1/2",
+        lat=51.2749006, lon=2.0602783
+    ),
+    # "POINT (1.6391558 51.3486548)",M 5/6,
+    M_5_6=LabeledPosition(
+        label="M 5/6",
+        lat=51.3486548, lon=1.6391558
+    ),
+    # "POINT (2.4790661 51.200747)",M 1/6,
+    M_1_6=LabeledPosition(
+        label="M 1/6",
+        lat=51.200747, lon=2.4790661
+    ),
+    # "POINT (2.1533663 51.6252596)",E 1/2,
+    E_1_2=LabeledPosition(
+        label="E 1/2",
+        lat=51.6252596, lon=2.1533663
+    ),
+    # "POINT (1.7656382 51.9317262)",E 5/6,
+    E_5_6=LabeledPosition(
+        label="E 5/6",
+        lat=51.9317262, lon=1.7656382
+    ),
+    # "POINT (1.9593153 51.7792136)",E 2/3,
+    E_2_3=LabeledPosition(
+        label="E 2/3",
+        lat=51.7792136, lon=1.9593153
+    ),
+    # "POINT (2.3450114 51.4696367)",E 1/3,
+    E_1_3=LabeledPosition(
+        label="E 1/3",
+        lat=51.4696367, lon=2.3450114
+    ),
+    # "POINT (2.5389216 51.3109382)",E 1/6,
+    E_1_6=LabeledPosition(
+        label="E 1/6",
+        lat=51.3109382, lon=2.5389216
+    ),
 )
 
 
